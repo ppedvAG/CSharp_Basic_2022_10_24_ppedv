429b604b52098e847a9de4bb8bc11653ea1bdbff
ppedvLukasK <lukask@ppedv.de>
2022-10-25 14:43:33 +0200

    M007

---
+++ M007/Program.cs
@@ -0,0 +1,72 @@
+﻿using System.Drawing;
+
+namespace M007
+{
+	internal class Program
+	{
+		static void Main(string[] args)
+		{
+			#region GC
+			for (int i = 0; i < 10; i++)
+			{
+				Window w = new Window();
+			}
+
+			GC.Collect(); //Force GC here
+			GC.WaitForPendingFinalizers(); //Wait for finalizers
+			#endregion
+
+			#region Static
+			//Static members work without objects
+			//Console c = new Console(); not possible
+			//c.WriteLine(); not possible
+			//Console.WriteLine();
+
+			Window w2 = new Window();
+			//w2.Counter not possible because static
+			Console.WriteLine(Window.Counter);
+			//w2.CountWindow(); not possible because static
+			Window.CountWindow();
+			#endregion
+
+			#region Value- and Referencetypes
+			//Valuetype
+			int original = 5;
+			int x = original; //Value gets copied (5)
+			original = 10; //No reference, so x is unchanged
+
+			//Referencetypes
+			Window w3 = new Window();
+			Window w4 = w3; //Creates a reference
+			w3.Width = 9; //Both windows now have a width of 9
+
+			Console.WriteLine(w3.GetHashCode()); //Same memory address
+			Console.WriteLine(w4.GetHashCode()); //Same memory address
+
+			//class: Referencetype, struct: Valuetype
+			#endregion
+
+			#region Null
+			Window w5; //Default value: null
+			w5 = null; //Clear a variable
+
+			//w5.OpenWindow(); no instance on w5 so I cannot open the window
+			if (w5 == null) //Check if object is null
+			{
+				Console.WriteLine("Object is null");
+				w5 = new Window();
+			}
+
+			if (w5 != null) //Check if the object exists
+			{
+				w5.OpenWindow();
+			}
+
+			if (w5 is null || w5 is not null) //Same as == and !=
+			{
+
+			}
+			#endregion
+		}
+	}
+}
---
+++ M007/Window.cs
@@ -0,0 +1,109 @@
+﻿namespace M007;
+
+internal class Window
+{
+	#region Variable + Get/Set
+	private double length; //Fields should not be accessible from the outside
+
+	/// <summary>
+	/// Returns the length of the window.
+	/// </summary>
+	/// <returns>The length of the window in meters.</returns>
+	public double GetLength()
+	{
+		return length;
+	}
+
+	/// <summary>
+	/// Sets the length of the window to a new value.
+	/// </summary>
+	/// <param name="length">The new length of the window in meters (0.1m to 10m).</param>
+	public void SetLength(double length)
+	{
+		if (length > 0.1 && length < 10) //Check before the value is set
+			this.length = length; //this: point outside of the method (take the upper length field)
+		else
+			Console.WriteLine("The new length is not valid");
+	}
+	#endregion
+
+	#region Properties
+	private double width;
+
+	public double Width
+	{
+		get { return width; }
+		set //We can also do some checks here
+		{
+			if (value > 0.1 && value < 10)
+				width = value; //value: The new value (like above in the set method -> length)
+			else
+				Console.WriteLine("The new length is not valid");
+		}
+	}
+
+	private WindowState state = WindowState.Closed; //Set the default value
+
+	public WindowState State
+	{
+		get { return state; }
+		private set { state = value; } //Private set: Make the property not settable from the outside (only inside of this class)
+	}
+
+	public double Area
+	{
+		get { return length * width; } //Get-only property (no set accessor)
+	}
+	#endregion
+
+	/// <summary>
+	/// A method that opens the window.
+	/// </summary>
+	public void OpenWindow() //Object will also have all functions that are defined in the class
+	{
+		if (state == WindowState.Closed)
+			state = WindowState.Opened;
+		else
+			Console.WriteLine("The window is already open");
+	}
+
+	#region Constructor
+	public Window() { } //Default constructor, every object has one
+
+	//Constructor: Sets the default values of the object at creation
+	public Window(double length, double width) //The default constructor gets deleted
+	{
+		SetLength(length); //Set the default values
+		Width = width;
+		//this.width = width; //Circumvents the checks from the property
+	}
+
+	//With : this we can chain constructors together
+	//if I execute this constructor the upper one will be executed as well
+	public Window(double length, double width, WindowState state) : this(length, width)
+	{
+		this.state = state;
+	}
+	#endregion
+
+	~Window() //~ + Tab + Tab
+	{
+		Console.WriteLine($"Window was collected {GetHashCode()}"); //Gets called when the garbage collector destroys this object
+		//GetHashCode(): Gets the memory address of the object
+	}
+
+	public static int Counter { get; set; } //static property is attached the class and not to the object
+
+	public static void CountWindow() //static method is attached to the class
+	{
+		Counter++;
+		//Console.WriteLine(length);
+		//OpenWindow(); not possible, because we don't have an object here
+	}
+}
+
+enum WindowState
+{
+	Closed,
+	Opened
+}
